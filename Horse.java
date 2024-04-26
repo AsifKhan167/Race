e8c433503389c21fa791af1bd77c06867f16717d
AsifKhan167 <163311529+AsifKhan167@users.noreply.github.com>
2024-04-26 19:14:47 +0100

    First iteration of Horse and Race codes

---
+++ Horse.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+
+public class Horse {
+    private String name;
+    private char symbol;
+    private int distanceTravelled;
+    private boolean fallen;
+    private double confidence;
+    private Random random = new Random();  // Add a Random object for generating random numbers
+
+    public Horse(char horseSymbol, String horseName, double horseConfidence) {
+        this.symbol = horseSymbol;
+        this.name = horseName;
+        this.confidence = horseConfidence;
+        this.distanceTravelled = 0;
+        this.fallen = false;
+    }
+
+    public void moveForward() {
+        if (!fallen) {
+            // Use confidence and randomness to affect movement; higher confidence has a higher chance to move further
+            if (random.nextDouble() < confidence) {
+                distanceTravelled += random.nextInt(3) + 1;  // Moves 1 to 3 steps forward based on a random value
+            }
+            // Introduce a chance of falling based on confidence (higher confidence has a slight risk of falling)
+            if (random.nextDouble() < (0.1 * (1 - confidence))) {
+                fallen = true;
+            }
+        }
+    }
+
+    public void fall() {
+        this.fallen = true;
+    }
+
+    public double getConfidence() {
+        return confidence;
+    }
+
+    public int getDistanceTravelled() {
+        return distanceTravelled;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public void goBackToStart() {
+        this.distanceTravelled = 0;
+    }
+
+    public boolean hasFallen() {
+        return fallen;
+    }
+
+    public void setConfidence(double newConfidence) {
+        this.confidence = newConfidence;
+    }
+
+    public void setSymbol(char newSymbol) {
+        this.symbol = newSymbol;
+    }
+}
---
+++ Race.java
@@ -0,0 +1,78 @@
+public class Race {
+    private Horse[] horses;
+    private int raceLength;
+
+    public Race(int raceLength, Horse[] horses) {
+        this.raceLength = raceLength;
+        this.horses = horses;
+    }
+
+    public void startRace() {
+        boolean raceFinished = false;
+        while (!raceFinished) {
+            raceFinished = true; // Assume race finishes unless a horse still needs to move
+            for (Horse horse : horses) {
+                if (!horse.hasFallen() && horse.getDistanceTravelled() < raceLength) {
+                    horse.moveForward();
+                    raceFinished = false; // If any horse moves, the race isn't finished
+                }
+            }
+            displayRaceStatus();
+            try {
+                Thread.sleep(500); // Pause for half a second between each race update for better visualization
+            } catch (InterruptedException e) {
+                System.out.println("Race thread interrupted.");
+            }
+        }
+        Horse winner = determineWinner();
+        if (winner != null) {
+            System.out.println("\nAnd the winner is " + winner.getName() + "!");
+        } else {
+            System.out.println("\nNo winner, all horses have fallen!");
+        }
+    }
+
+    private Horse determineWinner() {
+        Horse winningHorse = null;
+        int maxDistance = 0;
+        for (Horse horse : horses) {
+            if (!horse.hasFallen() && horse.getDistanceTravelled() > maxDistance) {
+                maxDistance = horse.getDistanceTravelled();
+                winningHorse = horse;
+            }
+        }
+        return winningHorse;
+    }
+
+    private void displayRaceStatus() {
+        System.out.println("\nRace progress:");
+        for (Horse horse : horses) {
+            if (horse.hasFallen()) {
+                System.out.printf("%s has fallen!\n", horse.getName());
+            } else {
+                int position = horse.getDistanceTravelled();
+                StringBuilder track = new StringBuilder("|");
+                for (int i = 0; i < raceLength; i++) {
+                    if (i == position) {
+                        track.append(horse.getSymbol());
+                    } else {
+                        track.append(".");
+                    }
+                }
+                track.append("| " + horse.getName() + " (Confidence: " + horse.getConfidence() + ")");
+                System.out.println(track);
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Horse[] horses = new Horse[3];
+        horses[0] = new Horse('P', "PIPPI LONGSTOCKING", 0.6);
+        horses[1] = new Horse('K', "KOKOMO", 0.6);
+        horses[2] = new Horse('E', "EL JEFE", 0.4);
+
+        Race race = new Race(50, horses);  // Assuming the race length is 50 meters
+        race.startRace();
+    }
+}
